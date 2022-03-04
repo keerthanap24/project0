b38c4ebe7c57572acc286be7f861f6b99cd4c037
keerthanap24 <61608170+keerthanap24@users.noreply.github.com>
2022-03-04 12:54:32 -0500

    third 5 line commit

--- project0-2.s
+++ project0-2.s
@@ -8,3 +8,8 @@ main:
     li $s0, 5           # N = 02933771 % 11 = 5
     la $a0, out_string  # load string address
     li $t0, 0           # $t0 is the counter for the loop
+loop:
+    beq $t0, $s0, exit  # loop conditional
+    syscall             # print [out_string]
+    addi $t0, $t0, 1    # increment $t0
+    j loop              # jump to the start of the loop
