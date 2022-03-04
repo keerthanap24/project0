05a050252ef2769e51e69fb91731b23a640ebf47
keerthanap24 <61608170+keerthanap24@users.noreply.github.com>
2022-03-04 12:53:46 -0500

    second 5 line commit

--- project0-2.s
+++ project0-2.s
@@ -3,3 +3,8 @@
     .data
     out_string: .asciiz "Hello @02933771\n"
     .text
+main:
+    li $v0, 4           # print string
+    li $s0, 5           # N = 02933771 % 11 = 5
+    la $a0, out_string  # load string address
+    li $t0, 0           # $t0 is the counter for the loop
